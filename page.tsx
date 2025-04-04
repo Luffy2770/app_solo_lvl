81c5d5b50d54718f7846bf3e79cd02533844e77b
Luffy2770 <manmayoza27@gmail.com>
2025-04-04 08:34:31 -0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white text-3xl">
-      <h1>Welcome to the System, Lowly Human</h1>
-    </div>
-  );
-}
