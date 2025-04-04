d7401705cf794aead5cb172092cb3cf261165a78
Luffy2770 <manmayoza27@gmail.com>
2025-04-04 08:34:49 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,7 @@
-
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black text-white text-3xl">
+      <h1>Welcome to the System, Lowly Human</h1>
+    </div>
+  );
+}
